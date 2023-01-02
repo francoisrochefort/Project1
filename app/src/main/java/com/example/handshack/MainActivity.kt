b6c4e78c1cf4e71c950ab07d5e5aba674ac06145
author <francoisrochefort@hotmail.fr>
2023-01-02 13:49:32 -0500

    adding something

--- app/src/main/java/com/example/handshack/MainActivity.kt
+++ app/src/main/java/com/example/handshack/MainActivity.kt
@@ -6,7 +6,6 @@ import android.content.Context
 import android.content.Intent
 import android.hardware.usb.UsbManager
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
