6c88bb3dead044c8f551a9e2ec69adf1cab5774f
author <francoisrochefort@hotmail.fr>
2023-03-06 18:37:27 -0500

    removed useless comments

--- misc/firmwares/ListAll/src/bucketrepository.cpp
+++ misc/firmwares/ListAll/src/bucketrepository.cpp
@@ -54,13 +54,11 @@ Result BucketRepository::setLimitSettings(const int id, const LimitSettings* set
     if (!db.limitSettingsExist(id)) {
 
         // Insert limit settings
-        Serial.println("j'ajoute les ti-samples limit_settings");
         db.addLimitSettings(id, settings);
     }
     else {
 
         // Update limit settings
-        Serial.println("j'update les ti-samples limit_settings");
         db.updateLimitSettings(id, settings);
     }
     return Succeeded;
@@ -79,13 +77,11 @@ Result BucketRepository::setC0Rising(const int id, const CalibrationSample* samp
     if (!db.c0RisingExists(id)) {
 
         // Insert bucket calibration
-        Serial.println("j'ajoute les ti-samples c0_rising");
         db.addC0Rising(id, samples);
     }
     else {
 
         // Update bucket calibration
-        Serial.println("j'update les ti-samples c0_rising");
         db.updateC0Rising(id, samples);
     }
     return Succeeded;
@@ -104,13 +100,11 @@ Result BucketRepository::setC0Lowering(const int id, const CalibrationSample* sa
     if (!db.c0LoweringExists(id)) {
 
         // Insert bucket calibration
-        Serial.println("j'ajoute les ti-samples c0_lowering");
         db.addC0Lowering(id, samples);
     }  
     else {
 
         // Update bucket calibration
-        Serial.println("j'update les ti-samples  c0_lowering");
         db.updateC0Lowering(id, samples);
     }
     return Succeeded;
@@ -129,13 +123,11 @@ Result BucketRepository::setX1Rising(const int id, const CalibrationSample* samp
     if (!db.x1RisingExists(id)) {
 
         // Insert bucket calibration
-        Serial.println("j'ajoute les ti-samples x1_rising");
         db.addX1Rising(id, samples);
     }
     else {
 
         // Update bucket calibration
-        Serial.println("j'update les ti-samples x1_rising");
         db.updateX1Rising(id, samples);
     }
     return Succeeded;
@@ -154,13 +146,11 @@ Result BucketRepository::setX1Lowering(const int id, const CalibrationSample* sa
     if (!db.x1LoweringExists(id)) {
 
         // Insert bucket calibration
-        Serial.println("j'ajoute les ti-samples x1_lowering");
         db.addX1Lowering(id, samples);
     }  
     else {
 
         // Update bucket calibration
-        Serial.println("j'update les ti-samples  x1_lowering");
         db.updateX1Lowering(id, samples);
     }
     return Succeeded;
@@ -179,13 +169,11 @@ Result BucketRepository::setC0LowSpeedFactor(const int id, const int* factors)
     if (!db.c0LowSpeedFactorExists(id)) {
 
         // Insert bucket calibration
-        Serial.println("j'ajoute les ti-factors c0_low_speed_factor");
         db.addC0LowSpeedFactor(id, factors);
     }  
     else {
 
         // Update bucket calibration
-        Serial.println("j'update les ti-factors  c0_low_speed_factor");
         db.updateC0LowSpeedFactor(id, factors);
     }
     return Succeeded;
@@ -204,13 +192,11 @@ Result BucketRepository::setC0HighSpeedFactor(const int id, const int* factors)
     if (!db.c0HighSpeedFactorExists(id)) {
 
         // Insert bucket calibration
-        Serial.println("j'ajoute les ti-factors c0_high_speed_factor");
         db.addC0HighSpeedFactor(id, factors);
     }  
     else {
 
         // Update bucket calibration
-        Serial.println("j'update les ti-factors  c0_high_speed_factor");
         db.updateC0HighSpeedFactor(id, factors);
     }
     return Succeeded;
@@ -229,13 +215,11 @@ Result BucketRepository::setX1LowSpeedFactor(const int id, const int* factors)
     if (!db.x1LowSpeedFactorExists(id)) {
 
         // Insert bucket calibration
-        Serial.println("j'ajoute les ti-factors x1_high_speed_factor");
         db.addX1LowSpeedFactor(id, factors);
     }  
     else {
 
         // Update bucket calibration
-        Serial.println("j'update les ti-factors  x1_high_speed_factor");
         db.updateX1LowSpeedFactor(id, factors);
     }
     return Succeeded;
@@ -254,13 +238,11 @@ Result BucketRepository::setX1HighSpeedFactor(const int id, const int* factors)
     if (!db.x1HighSpeedFactorExists(id)) {
 
         // Insert bucket calibration
-        Serial.println("j'ajoute les ti-factors x1_high_speed_factor");
         db.addX1HighSpeedFactor(id, factors);
     }  
     else {
 
         // Update bucket calibration
-        Serial.println("j'update les ti-factors  x1_high_speed_factor");
         db.updateX1HighSpeedFactor(id, factors);
     }
     return Succeeded;
